65e3a6bc44926bd62765889927183696ec93e988
Unknown <noah.stride@selfsown.com>
2018-10-21 13:57:59 +0100

    Fix login

--- src/pages/PageLogin.vue
+++ src/pages/PageLogin.vue
@@ -1,20 +1,21 @@
 <template>
-    <div id="PageLogin">
-        <div class="page-header">
-            <h1>Login
-                <small>Access our site with your Reddit account</small>
-            </h1>
-        </div>
-        <br>
-        <p> We only need your basic identity data from Reddit (Join Date, Username, User ID) and will not have access to your posts or have the ability to post as you.
-          By using OAuth2, we will never have access to your login credentials or any other personal information. However, by logging into the site you accept
-          our terms and conditions.</p>
-
-        <button type="button" class="btn btn-info" v-on:click="startLogin">Login with Reddit</button>
+  <div id="PageLogin">
+    <div class="page-header">
+      <h1>Login
+        <small>Access our site with your Reddit account</small>
+      </h1>
     </div>
+    <br>
+    <p> We only need your basic identity data from Reddit (Join Date, Username, User ID) and will not have access to your posts or have the ability to post as you.
+      By using OAuth2, we will never have access to your login credentials or any other personal information. However, by logging into the site you accept
+      our terms and conditions.</p>
+
+    <button type="button" class="btn btn-info" v-on:click="startLogin">Login with Reddit</button>
+  </div>
 </template>
 
 <script>
+import api from '@/api'
 export default {
   name: 'PageLogin',
   data: function () {
@@ -22,7 +23,7 @@ export default {
   },
   methods: {
     startLogin: function () {
-      window.location.href = '/api/auth/login'
+      window.location.href = api.defaults.baseURL + '/api/auth/login'
     }
   }
 }
