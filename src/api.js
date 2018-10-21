9c1dac23834a56808c948991ba988fa210b79a40
Unknown <noah.stride@selfsown.com>
2018-10-21 14:48:55 +0100

    cors

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://api.nub.international'
+  baseURL: 'https://api.nub.international',
+  withCredentials: true
 })
 
 export default api
