fbb1a5e60735bbda04e528407ec6372fb6beee79
Unknown <noah.stride@selfsown.com>
2018-10-23 18:11:25 +0100

    Only show GBP

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://api.nub.international' : 'http://127.0.0.1:9001',
+  baseURL: process.env.API_ENV || 'https://api.nub.international',
   withCredentials: true
 })
 
